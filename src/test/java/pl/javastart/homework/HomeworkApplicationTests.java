b3e041998335d35cef2de5608ef6c8c18eb56373
davidwrz <79601947+davidwrz@users.noreply.github.com>
2021-07-14 19:59:16 +0200

    Add files via upload

---
+++ src/test/java/pl/javastart/homework/HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.javastart.homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
